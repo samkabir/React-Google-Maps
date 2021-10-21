e39f3836afebd3823a7c37af01b72510fd1d09ba
samkabir <samkabir26@gmail.com>
2021-10-21 22:32:31 +0600

    First Commit

---
+++ src/Components/Map.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+// Cascading Style
+
+const containerStyle = {
+    width: '400vw',
+    height: '400vh'
+  };
+
+
+// LatLong
+const center = {
+    lat: 23.810331,
+    lng: 90.412521
+  };
+
+const Map = () => {
+    return (
+        <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+        >
+            <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={14}
+            >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+            </GoogleMap>
+        </LoadScript>
+    );
+};
+
+export default Map;
